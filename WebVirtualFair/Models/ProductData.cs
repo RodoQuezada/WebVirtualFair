f7430ca63015adf3a3a5b2c19f4a314d5507d6f9
Laura <lauragomez.glz@gmail.com>
2019-10-03 11:06:17 -0300

    Front de productos

---
+++ WebVirtualFair/Models/ProductData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebVirtualFair.Models
+{
+    public class ProductData
+    {
+        public long id { get; set; }
+       [Required(ErrorMessage = "Debe Ingresar Nombre del Producto")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "Debe Ingresar Descripción del Producto")]
+        public string description { get; set; }
+        [Required(ErrorMessage = "Debe Ingresar Imagen del Producto")]
+        public string image { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        
+        
+    }
+}
